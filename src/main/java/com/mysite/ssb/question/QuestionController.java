a7f87aeb82c3d6a72ee74406dd547a0e3a665a8c
고영광 <70497430+readable-ko@users.noreply.github.com>
2024-06-15 17:03:11 +0900

    last ppang

--- src/main/java/com/mysite/ssb/question/QuestionController.java
+++ src/main/java/com/mysite/ssb/question/QuestionController.java
@@ -6,6 +6,8 @@ import com.mysite.ssb.user.UserService;
 import jakarta.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/question")
 @Controller
@@ -31,6 +34,7 @@ public class QuestionController {
     @GetMapping("/list")
     public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
                        @RequestParam(value = "kw", defaultValue = "") String kw) {
+        log.info("page:{}, kw:{}",page,kw);
         Page<Question> paging = this.questionService.getList(page, kw);
         model.addAttribute("paging", paging);
         model.addAttribute("kw", kw);
